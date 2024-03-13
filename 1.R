b199a0757db491456448dadb714e532abf06606f
felix Zhao <felix.zhao@Felix-CCL-Macbook.local>
2024-03-14 10:55:57 +1300

    categories increase to 5 in demo

--- 1.R
+++ 1.R
@@ -1,5 +1,5 @@
 set.seed(1)
-long.df <- data.frame(Y=factor(sample(1:3,5*20,replace=TRUE)),
+long.df <- data.frame(Y=factor(sample(1:5,5*20,replace=TRUE)),
                 ROW=factor(rep(1:20,times=5)),COL=rep(1:5,each=20))
 
 # Model Log(P(Y=k)/P(Y=1))=mu_k+phi_k*rowc_coef_r with 3 row clustering groups:

fe1092baca9b15d305656e759de1e8932f1081ca
Felix Zhao <felixz2010@gmail.com>
2024-07-26 21:40:25 +1200

    ord from norm, 20 Y, 2 clusters, df

--- R/catchup_5/experiment_3/cluster_simulation_c5_3.R
+++ R/catchup_5/experiment_3/cluster_simulation_c5_3.R
@@ -2,6 +2,10 @@
 library(ggplot2)
 library(gridExtra)
 
+# parameters
+n <- 1000
+n_y <- 20
+
 # Set seed for reproducibility
 set.seed(123)
 
@@ -14,12 +18,12 @@ generate_ordinal_data <- function(n) {
   return(ordinal_data)
 }
 
-# Generate 20 datasets
-ordinal_datasets <- replicate(20, generate_ordinal_data(1000), simplify = FALSE)
+# Generate datasets
+ordinal_datasets <- replicate(n_y, generate_ordinal_data(n), simplify = FALSE)
 
 # Create a data frame
 ordinal_data_frame <- do.call(cbind, ordinal_datasets)
-colnames(ordinal_data_frame) <- paste0("Y", 1:20)
+colnames(ordinal_data_frame) <- paste0("Y", 1:n_y)
 
 # Print summary of the generated data frame
 print(summary(ordinal_data_frame))
@@ -27,7 +31,7 @@ print(summary(ordinal_data_frame))
 # Plot the first 10 datasets in a 3x4 grid of density plots
 plots <- list()
 for (i in 1:10) {
-  data <- data.frame(values = rnorm(1000))
+  data <- data.frame(values = rnorm(n))
   cuts <- quantile(data$values, probs = seq(0, 1, length.out = 4))
   data$ordinal <- cut(data$values, breaks = cuts, labels = c(0, 1, 2), include.lowest = TRUE)
   data$ordinal <- as.numeric(as.character(data$ordinal))
@@ -48,5 +52,10 @@ for (i in 1:10) {
 # Arrange the plots in a 3x4 grid
 grid.arrange(grobs = plots, ncol = 4)
 
+# Add random clusters
+cluster <- c(sample(1:2, n, replace = TRUE))
+
+df2 <- cbind(ordinal_data_frame, cluster)
+
 # Save the dataframe to a CSV file for further use if needed
-write.csv(ordinal_data_frame, "ordinal_data_frame.csv", row.names = FALSE)
+write.csv(df2, "./data/ordinal_data_frame.csv", row.names = FALSE)

49715368822118c674c509451ece5d897bb627ff
Felix Zhao <felixz2010@gmail.com>
2024-08-06 23:01:10 +1200

    half way to cluster dist sim, cut for each cluster diff, need to fix to same

--- R/catchup_6/experiment_3/cluster_dist_simulation_c6_3.R
+++ R/catchup_6/experiment_3/cluster_dist_simulation_c6_3.R
@@ -4,39 +4,52 @@ library(gridExtra)
 library(tidyr)
 
 # parameters
-n <- 1000
-n_y <- 20
+n <- 100
+n_y <- 1
 
 save_path <- paste0("./data/dist_simulation_y_", n_y, "_c6_3.csv")
 
 # Set seed for reproducibility
 set.seed(123)
 
-# Function to generate a vector of unique cuts starting from 0 and ending at 1
+# # Function to generate a vector of unique cuts starting from 0 and ending at 1
+# generate_cuts <- function(n) {
+#   if (n <= 2) {
+#     return(sort(c(0, 1)))
+#   }
+#   cuts <- unique(round(runif(n - 2, min = 0, max = 1), 2))
+#   while(length(cuts) < (n - 2)) {
+#     cuts <- unique(c(cuts, round(runif(n - length(cuts) - 2, min = 0, max = 1), 2)))
+#   }
+#   cuts <- sort(c(0, cuts[1:(n - 2)], 1))
+#   return(cuts)
+# }
+
+# Function to generate a vector of unique cuts from a uniform distribution
 generate_cuts <- function(n) {
-  if (n <= 2) {
-    return(sort(c(0, 1)))
+  cuts <- sort(runif(n - 2, min = 0, max = 1))
+  cuts <- unique(c(0, cuts, 1)) # Ensure cuts start from 0 and end at 1 and are unique
+  while(length(cuts) < n) {
+    # If the length of cuts is less than n, generate a new cut and add it
+    new_cut <- runif(1, min = 0, max = 1)
+    cuts <- sort(unique(c(cuts, new_cut)))
   }
-  cuts <- unique(round(runif(n - 2, min = 0, max = 1), 2))
-  while(length(cuts) < (n - 2)) {
-    cuts <- unique(c(cuts, round(runif(n - length(cuts) - 2, min = 0, max = 1), 2)))
-  }
-  cuts <- sort(c(0, cuts[1:(n - 2)], 1))
-  return(cuts)
+  return(round(cuts, 2))
 }
 
 # generate cuts prob vectors
-list_of_vectors <- lapply(1:20, function(x) generate_cuts(4))
+list_of_cuts <- lapply(1:20, function(x) generate_cuts(4))
 
-# cluster 1
-mean_cluster_1 <- 0
-var_cluster_1 <- 1
-n_cluster_1 <- n / 2
+
 
 # Function to generate ordinal data
 generate_ordinal_data <- function(n, mean, var, probs) {
   print(probs)
+  print(mean)
+  print(var)
   data <- rnorm(n, mean, var)
+  print(min(data))
+  print(max(data))
   cuts <- quantile(data, probs = probs)
   print(cuts)
   ordinal_data <- cut(data, breaks = cuts, labels = c(1, 2, 3), include.lowest = TRUE)
@@ -45,39 +58,64 @@ generate_ordinal_data <- function(n, mean, var, probs) {
 }
 
 # Generate datasets
-ordinal_datasets <- lapply(1:n_y, function(i) {
-  prob_vector <- list_of_vectors[[i]]
-  generate_ordinal_data(n_cluster_1, mean_cluster_1, var_cluster_1, prob_vector)
-}
+
+## Generate Cluster
+generate_cluster_data <- function(n, mean, var, list_of_probs, cluster_idx) {
+  ordinal_datasets <- lapply(1:n_y, function(i) {
+    prob_vector <- list_of_probs[[i]]
+    generate_ordinal_data(n, mean, var, prob_vector)
+  }
   )
+  
+  # Create a data frame
+  ordinal_data_frame <- do.call(cbind, ordinal_datasets)
+  colnames(ordinal_data_frame) <- paste0("Y", 1:n_y)
+  
+  # Print summary of the generated data frame
+  print(summary(ordinal_data_frame))
+  
+  # Add random clusters
+  cluster <- cluster_idx
+  
+  df2 <- cbind(ordinal_data_frame, cluster)
+  return(df2)
+}
 
-# Create a data frame
-ordinal_data_frame <- do.call(cbind, ordinal_datasets)
-colnames(ordinal_data_frame) <- paste0("Y", 1:n_y)
+## Plot Cluster
+plot_cluster <- function(df){
+  # Reshape the data to long format using pivot_longer
+  ordinal_data_long <- pivot_longer(as.data.frame(df[, 1:1]), 
+                                    cols = everything(), 
+                                    names_to = "Variable", 
+                                    values_to = "Value")
+  
+  # Plot density for the first 10 columns
+  p <- ggplot(ordinal_data_long, aes(x = Value)) +
+    geom_density(aes(fill = Variable), alpha = 0.3) +
+    labs(title = "Density Plots of the First 10 Columns", x = "Value", y = "Density") +
+    theme_minimal() +
+    theme(legend.position = "none") +
+    facet_wrap(~ Variable, ncol = 3)
+  
+  print(p)
+}
 
-# Print summary of the generated data frame
-print(summary(ordinal_data_frame))
+# Generate 
+# cluster 1
+mean_cluster_1 <- 0
+var_cluster_1 <- 1
+n_cluster_1 <- n / 2
+cluster_1_idx <- 1
+df_cluster1 <- generate_cluster_data(n_cluster_1, mean_cluster_1, var_cluster_1, list_of_cuts, cluster_1_idx)
+plot_cluster(df_cluster1)
 
-# Reshape the data to long format using pivot_longer
-ordinal_data_long <- pivot_longer(as.data.frame(ordinal_data_frame[, 1:10]), 
-                                  cols = everything(), 
-                                  names_to = "Variable", 
-                                  values_to = "Value")
-
-# Plot density for the first 10 columns
-p <- ggplot(ordinal_data_long, aes(x = Value)) +
-  geom_density(aes(fill = Variable), alpha = 0.3) +
-  labs(title = "Density Plots of the First 10 Columns", x = "Value", y = "Density") +
-  theme_minimal() +
-  theme(legend.position = "none") +
-  facet_wrap(~ Variable, ncol = 3)
-
-print(p)
-
-# Add random clusters
-cluster <- 1
-
-df2 <- cbind(ordinal_data_frame, cluster)
+# cluster 2
+mean_cluster_2 <- 30
+var_cluster_2 <- 60
+n_cluster_2 <- n / 2
+cluster_2_idx <- 2
+df_cluster2 <- generate_cluster_data(n_cluster_2, mean_cluster_2, var_cluster_2, list_of_cuts, cluster_2_idx)
+plot_cluster(df_cluster2)
 
 # Save the dataframe to a CSV file for further use if needed
 #write.csv(df2, save_path, row.names = FALSE)

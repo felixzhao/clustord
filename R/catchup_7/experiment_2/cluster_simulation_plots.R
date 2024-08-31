4b858f07e9ab419ce68ce7302ca51fee49a1f2ec
Felix Zhao <felixz2010@gmail.com>
2024-08-31 12:14:45 +1200

    workable para simulation plots

--- R/catchup_7/experiment_2/cluster_simulation_plots.R
+++ R/catchup_7/experiment_2/cluster_simulation_plots.R
@@ -9,41 +9,32 @@ set.seed(123)
 
 
 # functions
-generate_cluster_probs <- function(
-    G=2, 
-    q=3, 
-    alpha=c(1,-1), 
-    beta=c(0),
-    mu=c(0, 0.6, 0.3) ,
-    phi=c(0, 0.8, 1),
-    cluster_pi = c(0.3, 0.7),
-    sample_size = 1000,
-    total_sample_size = sample_size * G,
-    number_of_y = 1
-    ){
-  cluster_probs <- lapply(1:G, function(x) numeric(q))
+generate_cluster_probs <- function(G, q, alpha, beta, mu, phi, cluster_pi, 
+                                   sample_size, total_sample_size, number_of_y){
+  cluster_probs <- array(0,dim = c( G, number_of_y, q)) #lapply(1:G, function(x) numeric(q))
   
   for (g in 1:G) {
-    category_probs <- lapply(1:q, function(x) numeric(number_of_y))
-    for (k in 1:q) {
-      probs <- numeric(number_of_y)
-      for (j in 1: number_of_y) { # j loop must be out of k loop
-        linear <- mu[k] + phi[k] * (alpha[g] + beta[j])
+    #category_probs <- array() #list() #lapply(1:number_of_y, function(x) numeric(q))
+    for (j in 1: number_of_y) { # j loop must be out of k loop
+      #probs <- numeric(q)
+      for (k in 1:q) {
         if (k > 1) {
-          probs[j] <- exp(linear)
+          linear <- mu[k] + phi[k] * (alpha[g] + beta[j])
+          cluster_probs[g,j, k] <- exp(linear)
         } else {
-          probs[j] <- 1
+          cluster_probs[g,j, k] <- 1
         }
-        print(paste( g,k,j))
-        print(paste( mu[k], phi[k], alpha[g], beta[j]))
-        print(paste('linear', linear))
+        # print(paste( g,k,j))
+        # print(paste( mu[k], phi[k], alpha[g], beta[j]))
+        # print(paste('linear', linear))
       }
-      print(paste('probs', probs))
-      print(paste('norm probs', probs / sum(probs)))
-      category_probs[[k]] <- probs #/ sum(probs) # normalise k for each j # 2 dim, j, k
+      # print(paste('probs', probs))
+      # print(paste('norm probs', probs / sum(probs)))
+      cluster_probs[g,j,] <- cluster_probs[g,j,] / sum(cluster_probs[g,j,])
+      # category_probs[[j]] <- probs / sum(probs) # normalise k for each j # 2 dim, j, k
     }
-    sum_flattened_list <- sum(unlist(category_probs))
-    cluster_probs[[g]] <- category_probs # 3 dim, g, j, k
+    # sum_flattened_list <- sum(unlist(category_probs))
+    # cluster_probs[[g]] <- category_probs # 3 dim, g, j, k
   }
   return(cluster_probs)
 }
@@ -71,6 +62,7 @@ y_sampling <- function(sample_size, total_sample_size, cluster_pi, q, col_cluste
   data_list <- lapply(1:G, function(x) numeric(sample_size))
   
   for (g in 1:G) {
+    
     cluster_sample_size <- total_sample_size * cluster_pi[g]
     data_list[[g]] <- sample(1:q, size = cluster_sample_size, replace = TRUE, prob = col_cluster_probs[[g]])
   }
@@ -87,53 +79,32 @@ y_sampling <- function(sample_size, total_sample_size, cluster_pi, q, col_cluste
   names(samples)[1] <- paste0("Y", y_idx)
   return(samples)
 }
-
-data_samping <- function(G=2, 
-                         q=3, 
-                         alpha=c(1,-1), 
-                         beta=c(0),
-                         mu=c(0, 0.6, 0.3) ,
-                         phi=c(0, 0.8, 1),
-                         cluster_pi = c(0.3, 0.7),
-                         sample_size = 1000,
-                         total_sample_size = sample_size * G,
-                         number_of_y = 1) {
+data_samping <- function(sample_size, total_sample_size, cluster_pi, q, 
+                         cluster_probs, number_of_y) {
   
-  cluster_probs <- generate_cluster_probs(G,
-                                          q,
-                                          alpha,
-                                          beta,
-                                          mu,
-                                          phi,
-                                          cluster_pi,
-                                          sample_size,
-                                          total_sample_size,
-                                          number_of_y)
-  
+  data_list <- list()
+  clusters <- vector()
   # Create a list of N dataframes
-  dataframes <- lapply(1:number_of_y, function(i) {
-    col_cluster_probs <- lapply(cluster_probs, function(cluster) {
-      sapply(cluster, function(sublist) sublist[i])
-    })
-    y_sampling(sample_size, total_sample_size, cluster_pi, q, col_cluster_probs, i)
-  })
+  for (g in 1:G) {
+    cluster_sample_size <- total_sample_size * cluster_pi[g]
+    clust_matrix <- matrix(0, cluster_sample_size, number_of_y)
+    for (j in 1: number_of_y) {
+      clust_matrix[,j] <- sample(1:q, size = cluster_sample_size, replace = TRUE, prob = cluster_probs[g,j,])
+    }
+    data_list[[g]] <- clust_matrix
+    clusters <- c(clusters, rep(g, cluster_sample_size))
+  }
+  res1 <- do.call(rbind, data_list)
+  res2 <- as.data.frame(cbind(res1, clusters))
+  colnames(res2) <- c(paste0("Y",1:number_of_y), "cluster")
   
-  # Merge all dataframes
-  merged_df <- reduce(dataframes, function(df1, df2) {
-    inner_join(df1, df2, by = c("cluster", "id"))
-  })
-  
-  # Remove the unique identifier column
-  merged_df <- merged_df %>% select(-id)
-  
-  return(merged_df)
-  
+  return(res2)
 }
 
 # main
 G=2
 q=3
-alpha=c(-1,1)
+alpha=c(-3,3)
 beta=c(0)
 mu=c(0, 0.6, 0.3)
 phi=c(0, 0.8, 1)
@@ -148,8 +119,10 @@ number_of_y = 1
 # desc = paste('phi:',paste(phi, collapse = ", "))
 desc = paste('pi:',paste(cluster_pi, collapse = ", "))
 
-
-sample_df <- data_samping()
+cluster_probs <- generate_cluster_probs(G, q, alpha, beta, mu, phi, cluster_pi, 
+                                        sample_size, total_sample_size, number_of_y )
+sample_df <- data_samping(sample_size, total_sample_size, cluster_pi, q, 
+                          cluster_probs, number_of_y)
 plt_image <- plot_sample(sample_df, desc=desc)
 print(plt_image)
 

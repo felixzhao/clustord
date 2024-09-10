4faad1c1aa049ba05220e380c1c19fa9bbbacf48
Felix Zhao <felixz2010@gmail.com>
2024-09-10 21:43:25 +1200

    Add beta plots code

--- R/catchup_8/experiment_1/cluster_simulation_plots_c8_e1.R
+++ R/catchup_8/experiment_1/cluster_simulation_plots_c8_e1.R
@@ -96,19 +96,77 @@ cluster_probs_list <- lapply(alpha_list, function(alpha) {
 })
 batch_plots(alpha_list, 'alpha', cluster_probs_list)
 
+# ---------------------------#beta ------------------------
 # beta
-# reset_default_para_values()
-# number_of_y = 10
-# beta_list <- list(
-#   c(-0.1, 0.1),
-#   c(-1,1), 
-#   c(-3,3)
-# )
-# cluster_probs_list <- lapply(beta_list, function(beta) {
-#   generate_cluster_probs(G, q, alpha, beta, mu, phi, cluster_pi, 
-#                          sample_size, total_sample_size, number_of_y)
-# })
-# batch_plots(beta_list, 'beta', cluster_probs_list)
+reset_default_para_values()
+number_of_y = 5
+beta_list <- list(
+  c(-0.1, -0.05, 0, 0.05, 0.1),
+  c(-1, -0.5, 0, 0.5, 1),
+  c(-3, -1.5, 0, 1.5, 3)
+)
+cluster_probs_list <- lapply(beta_list, function(beta) {
+  generate_cluster_probs(G, q, alpha, beta, mu, phi, cluster_pi,
+                         sample_size, total_sample_size, number_of_y)
+})
+
+#----------------  beta plot
+para_name <- "beta"
+plots_title <- paste(default_plots_title, para_name)
+save_path <- paste0(default_save_path,para_name)
+beta_plots <- list()
+for (i in seq_along(beta_list)) {
+  cur_beta <- beta_list[[i]]
+  
+  sample_df <- data_samping(sample_size, total_sample_size, cluster_pi, q,
+                            cluster_probs_list[[i]], number_of_y)
+  y_plots <- list()
+  for (j in seq_along(beta_list[[1]])){
+    desc = paste(para_name,": ",paste(cur_beta[[j]], collapse = ", "))
+    plt_image <- plot_sample(sample_df, desc=desc, y_idx=j)
+    y_plots[[j]] <-plt_image
+  }
+  # show_plots(plots, plots_title, save_path)
+  beta_plots[[i]] <- y_plots
+}
+
+for (i in seq_along(beta_list)){
+  print(i)
+
+
+  # Extract Y-limits from all plots to determine the common range
+  y_limits <- range(unlist(lapply(beta_plots[[beta_idx]], function(plot) {
+    layer_data(plot)$y  # Extract the y-values from each plot
+  })))
+  
+  # Set the same Y-limits for each plot in beta_plots[[1]]
+  beta_plots_with_same_y <- lapply(beta_plots[[beta_idx]], function(plot) {
+    plot + ylim(y_limits)  # Add common ylim to each plot
+  })
+  
+  title <- paste0("Density Plots of Categories for Ys for Beta",
+                  paste(": ",paste(beta_list[[beta_idx]], collapse = ", ")))
+  arranged_plots <- grid.arrange(
+    grobs = beta_plots[[beta_idx]],
+    ncol = 5,
+    nrow = 1,
+    top = textGrob(title, gp = gpar(fontsize = 16, fontface = "bold"))
+  )
+  
+  # Display the arranged plots on the screen
+  print(arranged_plots)
+  
+  # save plot
+  file_path <- paste0(save_path, "_", beta_idx, ".png")
+  # Check if file_path is provided, then save the plot
+  if (!is.null(file_path) && file_path != "") {
+    ggsave(file_path, arranged_plots, width = 5 * 4, height = 1 * 4)
+  } else {
+    message("file_path is empty. Skipping the save step.")
+  }
+}
+
+# ---------------------------#beta end ------------------------
 
 # mu
 reset_default_para_values()

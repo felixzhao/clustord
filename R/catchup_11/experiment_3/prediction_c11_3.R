8670f49b99e744041060313946788a9550e0106c
Felix Zhao <felixz2010@gmail.com>
2024-10-13 21:05:46 +1300

    catchup 11 exps

---
+++ R/catchup_11/experiment_3/prediction_c11_3.R
@@ -0,0 +1,178 @@
+# install.packages("caret")
+# install.packages("dplyr")
+
+# Load necessary library
+library(dplyr)
+library("clustord")
+library(caret)
+
+library(ggplot2)
+library(gridExtra)
+library(grid)
+
+set.seed(123)
+
+source("R/catchup_9/osm_col_effect_functions.R")
+
+# arguments
+number_of_y <- 50
+df_path = paste0("./data/simulation_y_",number_of_y,"_c11_3.csv")
+
+# funtions
+## read data
+load_data <- function(data_path){
+  # Load the data from the CSV file
+  df <- read.csv(data_path, stringsAsFactors = FALSE)
+  
+  ### split train & test data
+  
+  
+  indexes <- sample(1:nrow(df), size = 0.7 * nrow(df))
+  
+  train_df <- df[indexes, ]
+  test_df <- df[-indexes, ]
+  
+  
+  ## data preparation
+  
+  ### train data
+  train_clust_df <- mat2df(mat = train_df %>% select(-cluster))
+  
+  ### test data
+  test_Y <- test_df %>% select(-cluster)
+  
+  test_cluster <- test_df["cluster"]
+  
+  return(list(train_df=train_clust_df, test_Y=test_Y, test_cluster=test_cluster))
+  
+}
+
+
+
+
+## training
+
+training <- function(df, number_of_y){
+  # training
+  # Model Log(P(Y=k)/P(Y=1))=mu_k+phi_k*rowc_coef_r with 2 row clustering groups:
+  results <- clustord(Y~ROWCLUST+COL,model="OSM",3,long.df=df, EM.control=list(EMcycles=100,startEMcycles=5), nstarts=5)
+  
+  
+  parlist <- results$parlist.out
+  cluster_pi <- results$pi.out
+  mu <- parlist$mu 
+  phi <- parlist$phi
+  alpha <- sort(parlist$rowc)
+  beta <- parlist$col
+  
+  print(mu)
+  print(phi)
+  print(alpha)
+  print(beta)
+  print(cluster_pi)
+  
+  probs <- get_cluster_prob_matrix(mu, phi, alpha, beta, cluster_pi, number_of_y)
+  print("Cluster Prob matrix:")
+  probs
+  
+  # col_clu_probs <- lapply(1:number_of_y, function(i) {
+  #        col_cluster_probs <-
+  #          lapply(probs, function(cluster) {
+  #              sapply(cluster, function(sublist) sublist[i])
+  #        })}) # g,j,k -> j,g,k
+
+  return(list(probs=probs, cluster_pi=cluster_pi, model=results))
+  
+}
+
+### prediction
+
+z_prediction <- function(y_i, model_probs, cluster_pi) {
+  row_probs <- lapply(1:length(y_i), function(x) numeric(length(cluster_pi)))
+  for (j in 1: number_of_y) {
+    # col_probs <- model_probs[,j,]
+    # probs_matrix <- do.call(rbind, col_probs)
+    # Extract columns based on y values and combine into matrix p
+    row_probs[[j]] <- t(sapply(y_i[j], function(k) model_probs[,j,k]))
+  }
+  
+  row_probs_matrix <- do.call(rbind, row_probs)
+  # Calculate the products of each column
+  z <- apply(row_probs_matrix, 2, prod)
+  
+  # Adjust each cluster's probabilities by multiplying with corresponding pi
+  adjusted_z <- cluster_pi * z
+    
+  #   mapply(function(cluster, p) {
+  #   cluster * p
+  # }, z, cluster_pi, SIMPLIFY = FALSE)
+  
+  return(adjusted_z)
+}
+
+cluster_prediction <- function(y, probs, cluster_pi){
+  z <- z_prediction(y, probs, cluster_pi)
+  # the sum of z for each y must be equal to 1
+  return(which.max(z))
+}
+
+prediction <- function(test_Y, model_probs, cluster_pi){
+  ## prediction all test data
+  predicted <- c()
+  
+  for (i in 1:nrow(test_Y)){
+    print(i)
+    new_obs <- as.numeric(as.character(test_Y[i,]))
+    predicted[i] <- cluster_prediction(new_obs, model_probs, cluster_pi)
+  }
+  return(predicted)
+}
+
+### evaluation 
+evaluation <- function(predicted, actual){
+  # confusion matrix
+  # Convert predicted and actual vectors to factors with the same levels
+  predicted <- factor(predicted, levels = c(1, 2, 3))
+  actual <- factor(actual, levels = c(1, 2, 3))
+  
+  # Calculate confusion matrix
+  conf_matrix <- confusionMatrix(predicted, actual)
+  
+  # return the confusion matrix
+  return(conf_matrix)
+}
+
+# main
+
+#main <- function(df_path, number_of_y){
+  data_dfs <- load_data(df_path)
+  train_df <- data_dfs$train_df
+  test_Y <- data_dfs$test_Y
+  test_cluster <- data_dfs$test_cluster$cluster
+  
+  model <- training(data_dfs$train_df, number_of_y)
+  
+  model_probs <- model$probs
+  cluster_pi <- model$cluster_pi
+  
+  predicted <- prediction(test_Y = test_Y, model_probs = model_probs, cluster_pi = cluster_pi)
+  
+  conf_matrix <- evaluation(predicted = predicted, actual = test_cluster)
+#}
+
+#conf_matrix <- main(df_path, number_of_y)
+print(conf_matrix)
+
+
+# n_of_y_list <- c(30, 50)
+# conf_matrix_list <- list()
+# 
+# for (number_of_y in n_of_y_list){
+#   print(number_of_y)
+#   
+#   data_path <- paste0("./data/simulation_y_",number_of_y,"_c4_1.csv")
+#   conf_matrix <- main(data_path)
+#   print(conf_matrix)
+#   conf_matrix_list[[paste(number_of_y)]] <- conf_matrix
+# }
+# 
---
+++ R/catchup_11/experiment_4/cluster_simulation_c11_4.R
@@ -0,0 +1,91 @@
+# install.packages("dplyr")
+
+library(rlang)
+library(ggplot2)
+library(gridExtra)
+library(grid)
+library(purrr)
+library(dplyr)
+
+set.seed(123)
+
+source("R/catchup_9/osm_col_effect_functions.R")
+# prob matrix for each Y, in current stage suppose all same.
+
+# default parameters
+number_of_y <- 50
+G <- 5
+alpha <- c(-1,-0.5,0,0.5,1)
+cluster_pi <- c(0.2, 0.2, 0.2, 0.2, 0.2)
+
+q <- 3
+beta <- runif(number_of_y, min = -1, max = 1) #c(-1, -0.5, 0, 0.5, 1)
+mu <- c(0, 0, 0)
+phi <- c(0, 0.5, 1)
+sample_size <- 2500
+total_sample_size <- sample_size * G
+
+
+
+cluster_probs <- get_cluster_prob_matrix(mu, phi, alpha, beta, cluster_pi, number_of_y)
+
+# sampling
+
+y_sampling <- function(sample_size, total_sample_size, cluster_pi, q, col_cluster_probs, y_idx) {
+  data_list <- lapply(1:G, function(x) numeric(sample_size))
+  
+  for (g in 1:G) {
+    
+    cluster_sample_size <- total_sample_size * cluster_pi[g]
+    data_list[[g]] <- sample(1:q, size = cluster_sample_size, replace = TRUE, prob = col_cluster_probs[[g]])
+  }
+  
+  # Flatten the list of lists into a single vector
+  data_val <- unlist(data_list)
+  
+  # Create a vector indicating the sublist number for each value
+  cluster_idx <- unlist(lapply(1:G, function(g) rep(g, length(data_list[[g]]))))
+  
+  # Creating a data frame for ggplot
+  samples <- data.frame(category = data_val, cluster = cluster_idx)
+  samples <- samples %>% mutate(id = row_number()) # Add a unique identifier
+  names(samples)[1] <- paste0("Y", y_idx)
+  return(samples)
+}
+
+data_samping <- function(sample_size, total_sample_size, cluster_pi, q, 
+                         cluster_probs, number_of_y) {
+
+  data_list <- list()
+  clusters <- vector()
+  # Create a list of N dataframes
+  for (g in 1:G) {
+    cluster_sample_size <- total_sample_size * cluster_pi[g]
+    clust_matrix <- matrix(0, cluster_sample_size, number_of_y)
+    for (j in 1: number_of_y) {
+      clust_matrix[,j] <- sample(1:q, size = cluster_sample_size, replace = TRUE, prob = cluster_probs[g,j,])
+    }
+    data_list[[g]] <- clust_matrix
+    clusters <- c(clusters, rep(g, cluster_sample_size))
+  }
+  res1 <- do.call(rbind, data_list)
+  res2 <- as.data.frame(cbind(res1, clusters))
+  colnames(res2) <- c(paste0("Y",1:number_of_y), "cluster")
+
+  return(res2)
+}
+
+save_data <- function(df, save_path = "./data/simulation_catgories_n_cluster_c9_1.csv"){
+  # save to csv file
+  write.csv(df, save_path , row.names=FALSE)
+  print(paste("Save data to",save_path,"Done."))
+}
+
+
+save_path <- paste0("./data/simulation_y_",number_of_y,"_c11_4.csv")
+
+df_y <- data_samping(sample_size, total_sample_size, cluster_pi, q, 
+                         cluster_probs, number_of_y
+                         )
+
+save_data(df_y, save_path = save_path)
